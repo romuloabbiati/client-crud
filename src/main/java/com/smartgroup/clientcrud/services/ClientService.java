fc0ad437be4e07b422586a810b73c061e19d6179
Romulo <rhabbiati@hotmail.com>
2022-10-08 09:13:42 +0100

    ClientService, ClientResource, findById update

--- src/main/java/com/smartgroup/clientcrud/services/ClientService.java
+++ src/main/java/com/smartgroup/clientcrud/services/ClientService.java
@@ -1,6 +1,7 @@
 package com.smartgroup.clientcrud.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.smartgroup.clientcrud.dto.ClientDTO;
+import com.smartgroup.clientcrud.entities.Client;
 import com.smartgroup.clientcrud.repositories.ClientRepository;
 
 @Service
@@ -21,6 +23,16 @@ public class ClientService {
 		return clientRepository.findAll().stream()
 			.map(client -> new ClientDTO(client))
 			.collect(Collectors.toList());
+	}
+	
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> clientOptional = clientRepository.findById(id);
+		
+		Client client = clientOptional.orElseThrow(
+				() -> new RuntimeException("Client not found!"));
+		
+		return new ClientDTO(client);
 	}
 	
 }
