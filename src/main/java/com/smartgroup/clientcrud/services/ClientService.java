d20b2850b070ee0fe9d6faf7bcadea2dcc2f9843
Romulo <rhabbiati@hotmail.com>
2022-10-08 08:24:47 +0100

    ClientDTO

--- src/main/java/com/smartgroup/clientcrud/services/ClientService.java
+++ src/main/java/com/smartgroup/clientcrud/services/ClientService.java
@@ -1,12 +1,13 @@
 package com.smartgroup.clientcrud.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.smartgroup.clientcrud.entities.Client;
+import com.smartgroup.clientcrud.dto.ClientDTO;
 import com.smartgroup.clientcrud.repositories.ClientRepository;
 
 @Service
@@ -16,8 +17,12 @@ public class ClientService {
 	private ClientRepository clientRepository;
 	
 	@Transactional(readOnly = true)
-	public List<Client> findAll() {
-		return clientRepository.findAll();
+	public List<ClientDTO> findAll() {
+		List<ClientDTO> clientsDTO = clientRepository.findAll().stream()
+			.map(client -> new ClientDTO(client))
+			.collect(Collectors.toList());
+		
+		return clientsDTO;
 	}
 	
 }
